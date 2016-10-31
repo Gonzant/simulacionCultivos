ecb4dd0945a3248d5f4331a8472db65db27a8e75
CarmenBarreira <csilbarreira@gmail.com>
2016-10-31 14:22:07 -0300

    validaciones MB
    falta probar mejor

--- webapp/static/js/validacionesMB.js
+++ webapp/static/js/validacionesMB.js
@@ -31,27 +31,27 @@ function validaPrecioGrano (){
 function validaPrecioFertilizante (){
 	var cantEscenarios = document.getElementsByName("precioFertilizanteEscenario").length;
 
-	var retorno; 
+	var retorno = [];  
 	for (var i=0; i < cantEscenarios; i=i+1){
 		
 		var precioFert = document.getElementsByName("precioFertilizanteEscenario")[i].value;		
 
 		if(parseInt(precioFert.length) != 0 ){ // no es vacio
-			retorno = true; 
+			retorno [i] = false;
 			if(parseFloat(precioFert) < 0 || parseFloat(precioFert) > 1000){// no se encuentra en el rango
 				document.getElementsByName("precioFertilizanteEscenario")[i].setAttribute('style', 'border-color: #a94442');
 				document.getElementsByName("precioFertilizanteEscenarioLE")[i].innerHTML = "El precio del fertilizante se debe encontrar entre 0 y 4";
-				retorno = false;
+				retorno [i]= false;
 			}
 			else{ //es correcto
 				document.getElementsByName("precioFertilizanteEscenario")[i].setAttribute('style', 'border-color: #ccc');
 				document.getElementsByName("precioFertilizanteEscenarioLE")[i].innerHTML =  " ";
-				retorno = true; 
+				retorno [i]= true; 
 			}	
 		}else{//es vacio	  		
 			document.getElementsByName("precioFertilizanteEscenario")[i].setAttribute('style', 'border-color: #a94442');
 			document.getElementsByName("precioFertilizanteEscenarioLE")[i].innerHTML = 'Este campo no puede ser vacio';
-			retorno = false;
+			retorno [i] = false;
 		}
 
 	}
@@ -61,24 +61,24 @@ function validaPrecioFertilizante (){
 function validaCostoRiego (){
 	
 	var cantEscenarios = document.getElementsByName("costeRiegoEscenario").length;	
-	var retorno = true; 
+	var retorno = [];  
 	for (var i=0; i < cantEscenarios; i=i+1){	
 		var costoRiegoEsc = (document.getElementsByName("costeRiegoEscenario")[i].value);	
 		if(parseInt(costoRiegoEsc.length) != 0 ){ // no es vacio
 			if(parseFloat(costoRiegoEsc) < 0 || parseFloat(costoRiegoEsc) > 1000){ 	// no se encuentra en el rango
 				document.getElementsByName("costeRiegoEscenario")[i].setAttribute('style', 'border-color: #a94442');
 				document.getElementsByName("costeRiegoEscenarioLE")[i].innerHTML = 'El costo del riego se debe encontrar entre 0 y 1000';
-				retorno = retorno && false;
+				retorno [i]= false;
 			}
 			else{ //es correcto
 				document.getElementsByName("costeRiegoEscenario")[i].setAttribute('style', 'border-color: #ccc');
 				document.getElementsByName("costeRiegoEscenarioLE")[i].innerHTML =' ';
-				retorno = retorno && true; 
+				retorno [i]=  true; 
 			}	
 		}else{//es vacio	  		
 				document.getElementsByName("costeRiegoEscenario")[i].setAttribute('style', 'border-color: #a94442');
 				document.getElementsByName("costeRiegoEscenarioLE")[i].innerHTML = 'Este campo no puede ser vacio';
-				retorno = retorno && false;
+				retorno [i]= false;
 		}
 
 	}
@@ -88,28 +88,27 @@ function validaCostoRiego (){
 function validaCostosGenerales (){
 	var cantEscenarios = document.getElementsByName("gastosGeneralesEscenario").length;
 	//alert (cantEscenarios);	
-	var retorno; 
+	var retorno = [];  
 	for (var i=0; i < cantEscenarios; i=i+1){
 		
 		var costosGen = (document.getElementsByName("gastosGeneralesEscenario")[i].value);		
 
 		if(parseInt(costosGen.length) != 0 ){ // no es vacio
-			retorno = true; 
 				if(parseFloat(costosGen) < 0 || parseFloat(costosGen) > 1000){
 				// no se encuentra en el rango
 					document.getElementsByName("gastosGeneralesEscenario")[i].setAttribute('style', 'border-color: #a94442');
 					document.getElementsByName("gastosGeneralesEscenarioLE")[i].innerHTML = 'Otros costos de producción se deben encontrar entre 300 y 1000';
-					retorno = false;
+					retorno[i] = false;
 				}
 				else{ //es correcto
 					document.getElementsByName("gastosGeneralesEscenario")[i].setAttribute('style', 'border-color: #ccc');
 					document.getElementsByName("gastosGeneralesEscenarioLE")[i].innerHTML= ' ';
-					retorno = true; 
+					retorno [i]= true; 
 				}	
 		}else{//es vacio	  		
 				document.getElementsByName("gastosGeneralesEscenario")[i].setAttribute('style', 'border-color: #a94442');
 				document.getElementsByName("gastosGeneralesEscenarioLE")[i].innerHTML ='Este campo no puede ser vacio';
-				retorno = false;
+				retorno [i]= false;
 		}
 
 	}
