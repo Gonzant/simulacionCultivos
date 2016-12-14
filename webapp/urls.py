c9e66ed1faddab602877719afc78e5314069d63b
CarmenBarreira <csilbarreira@gmail.com>
2016-12-14 12:52:00 -0300

    elimino carpeta simulacion y webapp

--- webapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.post_list),
-	url(r'^dssat$', views.run_dssat),	
-	url(r'^btnDSSAT$', views.btnDSSAT),
-	url(r'^margenBruto$', views.margenBruto),
-]
